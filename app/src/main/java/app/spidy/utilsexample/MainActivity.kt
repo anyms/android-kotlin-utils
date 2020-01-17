5f401c46eeab5fe4fbee918bc8c64298c1475d68
anyms <mytellee@gmail.com>
2020-01-17 23:17:32 +0530

    minor changes to code

--- app/src/main/java/app/spidy/utilsexample/MainActivity.kt
+++ app/src/main/java/app/spidy/utilsexample/MainActivity.kt
@@ -2,11 +2,14 @@ package app.spidy.utilsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.spidy.kotlinutils.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast("hello, world")
     }
 }
--- kotlinutils/src/main/java/app/spidy/kotlinutils/handyman.kt
+++ kotlinutils/src/main/java/app/spidy/kotlinutils/handyman.kt
@@ -7,13 +7,13 @@ import android.util.Log
 import android.widget.Toast
 import java.lang.Exception
 
-fun Context.toast(s: Any?, isLong: Boolean = false) {
+fun Context.toast(o: Any?, isLong: Boolean = false) {
     val duration = if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
-    Toast.makeText(this, s.toString(), duration).show()
+    Toast.makeText(this, o.toString(), duration).show()
 }
 
-fun String.debug(s: String) {
-    Log.d(this, s)
+fun String.debug(o: String) {
+    Log.d(this, o)
 }
 
 fun Context.onUiThread(callback: (context: Context) -> Unit) {
