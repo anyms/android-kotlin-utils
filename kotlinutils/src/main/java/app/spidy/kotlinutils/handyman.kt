a7473d808a54cf587064b52d38af781d5d0cf3e7
anyms <mytellee@gmail.com>
2020-02-06 23:31:27 +0530

    adding stacktraced logs

--- kotlinutils/src/main/java/app/spidy/kotlinutils/handyman.kt
+++ kotlinutils/src/main/java/app/spidy/kotlinutils/handyman.kt
@@ -7,13 +7,44 @@ import android.util.Log
 import android.widget.Toast
 import java.lang.Exception
 
+
+var DEBUG_MODE = false
+
 fun Context.toast(o: Any?, isLong: Boolean = false) {
     val duration = if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
     Toast.makeText(this, o.toString(), duration).show()
 }
 
-fun String.debug(o: String) {
-    Log.d(this, o)
+fun String.debug(o: Any?) {
+    if (DEBUG_MODE) {
+        Throwable().stackTrace[1].apply {
+            Log.d(this@debug, "[${this.fileName}] ${this@debug}: $o")
+        }
+    }
+}
+
+fun String.error(o: Any?) {
+    if (DEBUG_MODE) {
+        Throwable().stackTrace[1].apply {
+            Log.d(this@error, "[${this.fileName}] ${this@error}: $o")
+        }
+    }
+}
+
+fun String.warn(o: Any?) {
+    if (DEBUG_MODE) {
+        Throwable().stackTrace[1].apply {
+            Log.d(this@warn, "[${this.fileName}] ${this@warn}: $o")
+        }
+    }
+}
+
+fun String.info(o: Any?) {
+    if (DEBUG_MODE) {
+        Throwable().stackTrace[1].apply {
+            Log.d(this@info, "[${this.fileName}] ${this@info}: $o")
+        }
+    }
 }
 
 fun Context.onUiThread(callback: (context: Context) -> Unit) {
