e5c8657c0561571a89e6d66f5877f98b977212f7
anyms <mytellee@gmail.com>
2020-05-01 01:44:21 +0530

    adding Ask dialog

---
+++ kotlinutils/src/main/java/app/spidy/kotlinutils/Ask.kt
@@ -0,0 +1,60 @@
+package app.spidy.kotlinutils
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.View
+
+class Ask(private val context: Context) {
+    private var builder = AlertDialog.Builder(context)
+    private lateinit var alertDialog: AlertDialog
+    private var onCancelCallback: (() -> Unit)? = null
+    private var onOkCallback: (() -> Unit)? = null
+
+    var title: String = ""
+        set(value) {
+            builder.setTitle(value)
+        }
+    var message: String = ""
+        set(value) {
+            builder.setMessage(value)
+        }
+    var icon: Int = 0
+        set(value) {
+            builder.setIcon(value)
+        }
+    var customView: View? = null
+        set(value) {
+            if (value != null) {
+                builder.setView(value)
+            }
+        }
+    var isCancelable = false
+        set(value) {
+            builder.setCancelable(value)
+        }
+
+    fun show() {
+        if (!::alertDialog.isInitialized) {
+            alertDialog = builder.create()
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { _, _ ->
+                onOkCallback?.invoke()
+            }
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Cancel") { _, _ ->
+                onCancelCallback?.invoke()
+            }
+        }
+        alertDialog.show()
+    }
+
+    fun dismiss() {
+        alertDialog.dismiss()
+    }
+
+    fun onCancel(callback: () -> Unit) {
+        onCancelCallback = callback
+    }
+
+    fun onOk(callback: () -> Unit) {
+        onOkCallback = callback
+    }
+}
